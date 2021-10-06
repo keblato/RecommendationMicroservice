19bf1080966968852a7ca27ccb6ac3bca32bbd27
keblato <ks.blanco@uniandes.edu.co>
2021-10-06 04:24:02 -0500

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 import sys
 sys.path.insert(0,'/home/mobileApp/RecommendationMicroservice/src')
 
-from app.invalid_usage import InvalidUsage
+#from app.invalid_usage import InvalidUsage
 #from app.recommendations import genre_recommendations
 #from app.validation import validate_movieId
 app = Flask(__name__)
@@ -18,7 +18,7 @@ def index() -> str:
     # transform a dict into an application/json response 
     return jsonify({"message": "It Works"})
         
-@app.route("/movieR", methods=['POST'])
+""" @app.route("/movieR", methods=['POST'])
 def hello() -> str:
    errors = None
    if errors is not None:
@@ -27,18 +27,7 @@ def hello() -> str:
    movieId = request.json.get("movieId", None)
    response = "a2" #{"recommendations": list(genre_recommendations(movieId))}
    return jsonify(response)
-
-@app.route("/movieGOOD", methods=['GET'])
-def movieGOOD() -> str:
-
-   return True
-
-
-@app.errorhandler(InvalidUsage)
-def handle_invalid_usage(error):
-   response = jsonify(error.to_dict())
-   response.status_code = error.status_code
-   return response
+ """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')      
